490d31b38df30b51f6c91e02395bb49d088c9ee5
Muhammadreda264 <43699243+Muhammadreda264@users.noreply.github.com>
2020-08-11 17:39:23 +0200

    Update models.py

--- delivery/models.py
+++ delivery/models.py
@@ -18,63 +18,67 @@ class User(AbstractUser):
     )
     is_store = models.BooleanField(default=False)
     is_driver = models.BooleanField(default=False)
-    phone=models.CharField(max_length=30,verbose_name="رقم الجوال ")
+    phone = models.CharField(max_length=30, verbose_name="رقم الجوال ")
+
 
 class Store(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    adder=models.CharField(max_length=1000,default='',verbose_name="عنوان المتجر ")
-    name=models.CharField(max_length=500,verbose_name="اسم المتجر ")
+    adder = models.CharField(max_length=1000, default='', verbose_name="عنوان المتجر ")
+    name = models.CharField(max_length=500, verbose_name="اسم المتجر ")
+
     def __str__(self):
         return self.user.username
 
     class Meta:
         verbose_name_plural = "متاجر"
-        verbose_name='متجر'
+        verbose_name = 'متجر'
+
 
 class Driver(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    city=models.CharField(max_length=1000,default='')
-    name=models.CharField(max_length=500)
+    city = models.CharField(max_length=1000, default='')
+    name = models.CharField(max_length=500)
+
     def __str__(self):
         return f" {self.name}, {self.city}."
+
     class Meta:
         verbose_name_plural = "سائقين"
-        verbose_name='سائق'
+        verbose_name = 'سائق'
 
 
 class Order(models.Model):
-
-    store=models.ForeignKey(Store, on_delete=models.CASCADE)
-    driver=models.ForeignKey(Driver, on_delete=models.CASCADE,blank=True,null=True)
-    desc=models.TextField(default="",verbose_name="وصف الطلبية")
-    customername=models.CharField(max_length=300,default='',verbose_name="اسم العميل")
-    deliverfee=models.IntegerField(default=0)
-    orderfee =models.IntegerField(default=0,verbose_name="سعر الطلبية",help_text='اتركه فارغ لو كانت الطلبية مدفوعة')
-    created_at = models.DateTimeField(auto_now_add=True,blank=True)
-    phone = models.CharField(max_length=30,verbose_name="رقم جوال العميل")
-    adder = models.CharField(max_length=1000, default='',verbose_name="عنوان العميل ")
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, blank=True, null=True)
+    desc = models.TextField(default="", verbose_name="وصف الطلبية")
+    customername = models.CharField(max_length=300, default='', verbose_name="اسم العميل")
+    deliverfee = models.IntegerField(default=0)
+    orderfee = models.IntegerField(default=0, verbose_name="سعر الطلبية", help_text='اتركه فارغ لو كانت الطلبية مدفوعة')
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    phone = models.CharField(max_length=30, verbose_name="رقم جوال العميل")
+    adder = models.CharField(max_length=1000, default='', verbose_name="عنوان العميل ")
 
     class Status(models.TextChoices):
-        NEW ='NE', 'جديدة لم يعين سائق'
+        NEW = 'NE', 'جديدة لم يعين سائق'
         PENDING = 'PN', 'في انتظار وصول السائق للمتجر'
-        SHIPPED ='SH','تم الشحن للتسليم'
-        DELIVERED = 'PB', 'استلمها العميل'
-        CANCELLED='CA','ملغية'
+        SHIPPED = 'SH', 'تم الشحن للتسليم'
+        DELIVERED = 'DE', 'استلمها العميل'
+        CANCELLED = 'CA', 'ملغية'
 
     status = models.CharField(
         max_length=2,
         choices=Status.choices,
         default=Status.NEW,
     )
-    def save(self,*args,**kwargs):
-        if self.driver and self.status=='NE':
-            self.status='PN'
-        super().save(*args,**kwargs)
 
+    def save(self, *args, **kwargs):
+        if self.driver and self.status == 'NE':
+            self.status = 'PN'
+        super().save(*args, **kwargs)
 
+    def __str__(self):
+        return f" {self.store}."
 
     class Meta:
         verbose_name_plural = "طلبيات"
-        verbose_name='طلبية'
-
-
+        verbose_name = 'طلبية'
