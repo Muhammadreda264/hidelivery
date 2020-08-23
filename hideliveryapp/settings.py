891ea06a1428425755b67b78d13ae988792df17f
fantazy <mohamedreda26499@gmail.com>
2020-08-23 20:19:36 +0200

    migrate

--- hideliveryapp/settings.py
+++ hideliveryapp/settings.py
@@ -81,9 +81,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-if not DEBUG :
-    import dj_database_url
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
